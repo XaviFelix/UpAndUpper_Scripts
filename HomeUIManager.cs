ad4e0cb192764894ec276d0e98a89e8e6b016fa1
setiwon <45044735+setiwon@users.noreply.github.com>
2024-11-30 17:38:56 -0800

    Added program description

--- HomeUIManager.cs
+++ HomeUIManager.cs
@@ -1,3 +1,13 @@
+/***************************************************************
+*file: HomeUIManager.cs
+*author: Joseph Setiawan
+*class: CS 4700 - Game Development
+*assignment: Program 4
+*date last modified: 11/15/24
+*
+*purpose: This program handles the start button for the home menu.
+*         
+****************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +18,6 @@ public class HomeUIManager : MonoBehaviour
     // Start the game
     public void StartGame()
     {
-        SceneManager.LoadScene("Level1");  // Replace "Level1" with the actual name of  game scene
+        SceneManager.LoadScene("SampleScene"); 
     }
 }
